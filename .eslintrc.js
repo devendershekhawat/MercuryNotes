5960400b456b21688a95527b54c03f3bb8da141e
Devender Shekhawat <devender_shekhawat@external.mckinsey.com>
2022-05-06 11:02:23 +0530

    update prettier and eslint config to account for jsx code and linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,32 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "prettier",
-    "plugin:import/errors",
-    "plugin:jsx-a11y/recommended",
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "import", "jsx-a11y"],
-  rules: {
-    "react/prop-types": 0,
-    "react/react-in-jsx-scope": 0,
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'prettier',
+		'plugin:import/errors',
+		'plugin:jsx-a11y/recommended'
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['react', 'import', 'jsx-a11y'],
+	rules: {
+		'react/prop-types': 0,
+		'react/react-in-jsx-scope': 0,
+		quotes: [2, 'single', { avoidEscape: true }]
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	}
 };
